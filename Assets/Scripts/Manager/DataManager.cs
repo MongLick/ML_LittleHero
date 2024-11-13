6e78472007cda8a3f4d5fdcef0b96b3876d47345
MongLick <qkjm4836@gmail.com>
2024-11-13 20:12:42 +0900

    refactor : Modify Title Scene Panel

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -5,6 +5,4 @@ public class DataManager : Singleton<DataManager>
 	[Header("Components")]
 	[SerializeField] UserData userData;
 	public UserData UserData { get { return userData; } set { userData = value; } }
-	[SerializeField] InventorySlotData inventorySlotData;
-	public InventorySlotData InventorySlotData { get { return inventorySlotData; } set { inventorySlotData = value; } }
 }
